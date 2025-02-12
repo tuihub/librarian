1a4c7f1c6a961692108e23d135efbb1e67c95ca9
MuZhou233 <muzhou233@outlook.com>
2025-02-12 20:18:27 +0800

    fix

--- internal/lib/libsentry/sentry.go
+++ internal/lib/libsentry/sentry.go
@@ -12,12 +12,12 @@ func InitSentry(c *conf.Sentry) error {
 	}
 	// Initialize the Sentry SDK.
 	err := sentry.Init(sentry.ClientOptions{ //nolint:exhaustruct // no need
-		Dsn:                c.GetDsn(),
-		AttachStacktrace:   true,
-		EnableTracing:      true,
-		TracesSampleRate:   1.0,
-		ProfilesSampleRate: 1.0,
-		Environment:        c.GetEnvironment(),
+		Dsn:              c.GetDsn(),
+		AttachStacktrace: true,
+		EnableTracing:    true,
+		TracesSampleRate: 1.0,
+		SampleRate:       1.0,
+		Environment:      c.GetEnvironment(),
 	})
 	if err != nil {
 		return err
